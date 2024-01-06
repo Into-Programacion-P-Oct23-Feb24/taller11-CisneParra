0156aa0ada4c17115e1f08352c4df6be9faa3e93
Cisne Parra <adparra2@utpl.edu.ec>
2024-01-05 20:57:47 -0500

    cambios de la tarea

--- ejercicios/Problemas/src/problemas/Problema6.java
+++ ejercicios/Problemas/src/problemas/Problema6.java
@@ -5,10 +5,50 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema6 {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de elementos del arreglo: ");
+        int n = scanner.nextInt();
+        int[] informacion = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Ingrese un valor para el arreglo");
+            informacion[i] = scanner.nextInt();
+        }
+        double media = calcularMedia(informacion);
+        System.out.printf("La media aritmética es: %.2f\n", media);
+        double desviacionEstandar = calcularDesviacionEstandar(informacion,
+                media);
+        System.out.printf("La desviación estándar es: %.2f\n",
+                desviacionEstandar);
+    }
+
+    // Función para calcular la media aritmética
+    public static double calcularMedia(int[] arreglo) {
+        int suma = 0;
+        for (int i = 0; i < arreglo.length; i++) {
+            suma = suma + arreglo[i];
+        }
+        return (double) suma / arreglo.length;
+    }
+    public static double calcularDesviacionEstandar(int[] arreglo, double media) {
+        double suma = 0;
+        for (int i = 0; i < arreglo.length; i++) {
+            int num = arreglo[i];
+            double diferencia = num - media;
+            double diferenciaC = diferencia * diferencia;
+            suma = diferenciaC;
+        }
+        double varianza = suma / arreglo.length;
+        double desviacionEstandar = Math.sqrt(varianza);
+        return desviacionEstandar;
+    }
+
 }
--- ejercicios/Problemas/src/problemas/Problema7.java
+++ ejercicios/Problemas/src/problemas/Problema7.java
@@ -5,10 +5,41 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema7 {
-    
-}
+
+    public static void main(String[] args) {
+        String[] arreglo = crearArreglo();
+        imprimirvalores(arreglo);
+    }
+
+    public static String[] crearArreglo() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de valores del arreglo ");
+        int cantidad = scanner.nextInt();
+        scanner.nextLine();
+        String[] valores = new String[cantidad];
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println("Ingrese el dato que usted desee");
+            valores[i] = scanner.nextLine();
+        }
+        return valores;
+    }
+
+    public static void imprimirvalores(String[] valores) {
+        for (int i = 0; i < valores.length; i++) {
+        String valor = valores[i];
+        if (valor.length() == 4 || valor.length() == 5) {
+            System.out.printf("%s\n",valor);
+        }
+    }
+        }
+    }
+
+
